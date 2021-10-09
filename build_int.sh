1dc1e6444238e48745ec6cb127989cdadefb86aa
Bastian Bukatz <bastian.bukatz@uzh.ch>
2021-10-09 21:39:27 +0200

    addes user config

--- build_int.sh
+++ build_int.sh
@@ -6,4 +6,4 @@ pip install ansible docker
 
 ansible-galaxy collection install -f einfachit.epicserver
 ansible-playbook -i inventory_int.yml --extra-var project_dir=$(pwd) einfachit.epicserver.provision
-ansible-playbook -i inventory_int.yml --vault-password-file .vaultpass --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epicserver.configure
+ansible-playbook -i inventory_int.yml --vault-password-file .vaultpass --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epicserver.configure
