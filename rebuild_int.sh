1af45e20fe1bac31474145e24afa70aa17e82ffc
Bastian Bukatz <bastian.greuel@gmail.com>
2021-11-20 10:00:05 +0100

    set passwords for integration, for prod we will not commit then to the repo

--- rebuild_int.sh
+++ rebuild_int.sh
@@ -3,8 +3,8 @@
 source source.me
 
 ansible-galaxy collection install -f  einfachit.epicserver
-ansible-playbook -i inventory_int.yml einfachit.epicserver.decomission-server
-ansible-playbook -i inventory_int.yml einfachit.epicserver.provision-server
-ansible-playbook -i inventory_int.yml einfachit.epicserver.configure-server
-ansible-playbook -i inventory_int.yml --extra-var project_dir=$(pwd) einfachit.epicserver.provision
-ansible-playbook -i inventory_int.yml --vault-password-file .vaultpass --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epicserver.configure
+ansible-playbook -i inventory_int.yml einfachit.epicserver.decommission_server
+ansible-playbook -i inventory_int.yml einfachit.epicserver.provision_server
+ansible-playbook -i inventory_int.yml einfachit.epicserver.configure_server
+ansible-playbook -i inventory_int.yml --extra-var project_dir=$(pwd) einfachit.epicserver.provision_container
+ansible-playbook -i inventory_int.yml --vault-password-file .vaultpass --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epicserver.configure_container
