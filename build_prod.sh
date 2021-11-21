11bb6496f7b0ea322ef7e37c6affc14d9fc2f14c
Bastian Bukatz <bastian.greuel@gmail.com>
2021-11-21 10:50:45 +0100

    fixes #1

--- build_prod.sh
+++ build_prod.sh
@@ -4,7 +4,7 @@ virtualenv -p python3 venv
 source venv/bin/activate
 pip install ansible docker
 
-ansible-galaxy collection install -f einfachit.epicserver
-ansible-playbook -i inventory_prod.yml einfachit.epicserver.configure-server
-#ansible-playbook -i inventory_prod.yml --extra-var project_dir=$(pwd) einfachit.epicserver.provision
-#ansible-playbook -i inventory_prod.yml --vault-password-file .vaultpass --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epicserver.configure
+ansible-galaxy collection install -f einfachit.epic
+ansible-playbook -i inventory_prod.yml einfachit.epic.configure-server
+#ansible-playbook -i inventory_prod.yml --extra-var project_dir=$(pwd) einfachit.epic.provision
+#ansible-playbook -i inventory_prod.yml --vault-password-file .vaultpass --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epic.configure
