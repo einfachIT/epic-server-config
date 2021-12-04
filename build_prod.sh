13fdf5d432bed0906096c9e7b231efdca10c15a3
Bastian Bukatz <bastian.greuel@gmail.com>
2021-12-04 23:39:42 +0100

    added github action to run acceptance test

--- build_prod.sh
+++ build_prod.sh
@@ -7,4 +7,4 @@ pip install ansible docker
 ansible-galaxy collection install -f einfachit.epic
 ansible-playbook -i inventory_prod.yml einfachit.epic.configure-server
 #ansible-playbook -i inventory_prod.yml --extra-var project_dir=$(pwd) einfachit.epic.provision
-#ansible-playbook -i inventory_prod.yml --vault-password-file .vaultpass --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epic.configure
+#ansible-playbook -i inventory_prod.yml --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epic.configure
