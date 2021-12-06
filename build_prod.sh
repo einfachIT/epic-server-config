687ed1ac05dca56697d55cbecc3017cd7d6ab32c
Bastian Bukatz <bastian.greuel@gmail.com>
2021-12-06 10:26:28 +0100

    ignore know_hosts and added tim-thaler test epicraspberry

--- build_prod.sh
+++ build_prod.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-virtualenv -p python3 venv
-source venv/bin/activate
-pip install ansible docker
+source source.me
 
-ansible-galaxy collection install -f einfachit.epic
-ansible-playbook -i inventory_prod.yml einfachit.epic.configure-server
-#ansible-playbook -i inventory_prod.yml --extra-var project_dir=$(pwd) einfachit.epic.provision
-#ansible-playbook -i inventory_prod.yml --extra-var @users.yml --extra-var project_dir=$(pwd) --extra-var @secrets_int.yml einfachit.epic.configure
+ansible-galaxy collection install -f einfachIT.epic
+ansible-playbook -i inventory_prod.yml einfachit.epic.provision_raspberries
+ansible-playbook -i inventory_prod.yml einfachit.epic.configure_server
+ansible-playbook -i inventory_prod.yml --extra-var project_dir=$(pwd) einfachit.epic.provision_container
+ansible-playbook -i inventory_prod.yml --extra-var @users_prod.yml --extra-var project_dir=$(pwd) --extra-var @secrets_prod.yml einfachit.epic.configure_container
