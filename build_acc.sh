f112d1807665306382ed7548e40ba744fcefad8e
Bastian Bukatz <bastian.greuel@gmail.com>
2022-09-22 20:25:36 +0200

    added step to provision raspis, its part of the build

--- build_acc.sh
+++ build_acc.sh
@@ -3,6 +3,7 @@
 source source.me
 
 ansible-galaxy collection install -f git@github.com:einfachIT/ansible-collection-epic.git
+ansible-playbook -i inventory_acc.yml einfachit.epic.provision_raspberries
 ansible-playbook -i inventory_acc.yml einfachit.epic.configure_server
 ansible-playbook -i inventory_acc.yml --extra-var project_dir=$(pwd) einfachit.epic.provision_container
 ansible-playbook -i inventory_acc.yml --extra-var @users_acc.yml --extra-var project_dir=$(pwd) --extra-var @secrets_acc.yml einfachit.epic.configure_epicbackup
