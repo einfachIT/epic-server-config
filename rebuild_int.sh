105299bdf3f6067cba009c4c9c394dd64a921042
Bastian Bukatz <bastian.greuel@gmail.com>
2022-11-19 14:53:46 +0100

    some updates in inventory added script to built int (using git head instead of last published version of ansible collection)

---
+++ rebuild_int.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+source source.me
+
+ansible-galaxy collection install -f ../ansible-collection-epic 
+ansible-playbook -i inventory_acc.yml --extra-var project_dir=$(pwd) einfachit.epic.decommission_container
+ansible-playbook -i inventory_acc.yml einfachit.epic.decommission_raspberries
+ansible-playbook -i inventory_acc.yml einfachit.epic.provision_raspberries
+ansible-playbook -i inventory_acc.yml einfachit.epic.configure_server
+ansible-playbook -i inventory_acc.yml einfachit.epic.configure_epicraspberries
+ansible-playbook -i inventory_acc.yml --extra-var project_dir=$(pwd) einfachit.epic.provision_container
+ansible-playbook -i inventory_acc.yml --extra-var @users_acc.yml --extra-var project_dir=$(pwd) --extra-var @secrets_acc.yml einfachit.epic.configure_epicbackup
