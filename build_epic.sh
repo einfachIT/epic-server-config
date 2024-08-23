61dbda15e8023bebf4f7825ca8be0f75d50e8aeb
Bastian Bukatz <bastian.greuel@gmail.com>
2024-08-23 07:08:38 +0200

    setup for course

---
+++ build_epic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source source.me
+
+#ansible-galaxy collection install -f git@github.com:einfachIT/ansible-collection-epic.git
+#ansible-galaxy collection install -f git@github.com:einfachIT/ansible-collection-infra.git
+
+ansible-galaxy collection install -f ../ansible-collection-epic
+ansible-galaxy collection install -f ../ansible-collection-epic-infra
+
+ansible-playbook -i inventory_epic.yml einfachit.epic.provision_raspberries
+ansible-playbook -i inventory_epic.yml einfachit.epic.configure_server
+ansible-playbook -i inventory_epic.yml --extra-var project_dir=$(pwd) einfachit.epic.provision_container
+ansible-playbook -i inventory_epic.yml --extra-var @users_epic.yml --extra-var project_dir=$(pwd) --extra-var @secrets_epic.yml einfachit.epic.configure_epicbackup
+ansible-playbook -i inventory_epic.yml einfachit.hpc.configure_server
